227381223514a3dcdcfa1a10678e35443a98dc8d
Hekar <hekark@gmail.com>
2012-09-25 20:40:25 -0400

    final commit

---
+++ projects/plc/run.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+java -jar plc_server.jar %*
+@ECHO ON
--- scripts/setup.bat
+++
@@ -1,3 +0,0 @@
-echo "Downloading and unzipping Play Framework 2.0"
-
-java -cp "%dp0%filefetch/dist/*.jar" org.filefetch.Main "http://download.playframework.org/releases/play-2.0.3.zip"
